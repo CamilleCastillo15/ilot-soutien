9e73f60fdb23369948c96805cb160324130cb5c4
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-07-27 19:28:09 +0200

    wiwip

--- sites/all/themes/ilot/templates/node/node--accueil.tpl.php
+++ sites/all/themes/ilot/templates/node/node--accueil.tpl.php
@@ -5,12 +5,35 @@
   <section class="actualites">
     <h1 class="gros">actualités</h1>
     <?php print $views_accueil_actu; ?>
-    <?php print l('Voir toutes les actus', 'actualités', array('attributes' => array('class' => 'actu read-more')));?>
-  </section>
-  <section class="mosaique">
-    <?php print $views_accueil_mosaique_temoignage; ?>
-    <?php print $views_accueil_mosaique_projet; ?>
-    <a href="/faire-un-don" class="faire-un-don">Faire un don</a>
-  </section>
+    <?php print l('Voir toutes les actus', 'actualités', array('attributes' => array('class' => 'actu readmore')));?>
+  </section><section class="mosaique-new">
+    <div class="bloc bloc-text dernier-projet">
+        <div class="mosaique-text">denier projet</div>
+        <div class="arrow east"></div>
+    </div><div class="bloc bloc-img img-dernier-projet">
+       <img src="sites/all/themes/ilot/images/img_test/samia-temoignage.png" />
+    </div><!--
+       --><div class="bloc bloc-img img-son-temoignage">
+        <img src="sites/all/themes/ilot/images/img_test/bel-espoir.png" />
+    </div><div class="bloc bloc-text son-temoignage">
+        <div class="mosaique-text">son témoignage</div>
+        <div class="arrow west"></div>
+    </div><!--
+       --><div class="bloc bloc-text espace-presse">
+        <div class="mosaique-text">espace presse</div>
+        <div class="arrow east"></div>
+    </div><div class="bloc bloc-img img-espace-presse"><img src="sites/all/themes/ilot/images/img_test/samia-temoignage.png" /></div><!--
+       --><div class="bloc bloc-img img-lot-media"><img src="sites/all/themes/ilot/images/img_test/bel-espoir.png" />
+    </div><div class="bloc bloc-text ilot-medias">
+        <div class="mosaique-text">l'îlot dans les médias</div>
+        <div class="arrow west"></div>
+    </div>
+</section>
 </article><!-- /.node -->
 <div class="clearfix"></div>
+
+  <!-- <section class="mosaique">
+    <?php //print $views_accueil_mosaique_temoignage; ?>
+    <?php //print $views_accueil_mosaique_projet; ?>
+      <a href="/faire-un-don" class="faire-un-don">Faire un don</a>
+  </section> -->
--- sites/all/themes/ilot/templates/node/node--actualites--teaser.tpl.php
+++ sites/all/themes/ilot/templates/node/node--actualites--teaser.tpl.php
@@ -4,7 +4,7 @@
         <a href="<?php print url('node/' . $node->nid); ?>">
         <?php  print render($content['field_actualite_image_accroche']); ?>
         </a>
-        <span class="auteur"><?php if(!empty($auteur)){print $auteur;} ?></span>
+        <!-- <span class="auteur"><?php //if(!empty($auteur)){print $auteur;} ?></span> -->
       </div>
       <div class="date">
             <?php print $actualites_date; ?>
@@ -16,7 +16,17 @@
       <div class="text-actus texte">
         <h1><?php print l($node->title, 'node/'.$node->nid, array('html' => TRUE)); ?></h1>
         <div class="text-detail">
-            <?php  print render($content['field_actualite_accroche']); ?>
+            <?php  /*
+
+                $accroche = field_view_field("node",$node,'field_actualite_accroche',array(
+                  'label'=>'hidden',
+                  'type' => 'text_summary_or_trimmed',
+                  'settings'=>array('trim_length' => 50),
+            ));
+
+            print render($accroche);*/
+            print render($content['field_actualite_accroche']);?>
+
         </div>
         <div class="readmore">
             <?php print l('voir plus', 'node/'.$node->nid, array('html' => TRUE, 'attributes' => array('class' => array('read-more')))); ?>
