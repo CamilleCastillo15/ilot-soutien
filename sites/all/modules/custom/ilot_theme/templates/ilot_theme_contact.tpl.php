d5abfb7af745e1f264542c430a3832eba7b628bb
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-07-28 18:03:06 +0200

    wiwipo

---
+++ sites/all/modules/custom/ilot_theme/templates/ilot_theme_contact.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+//    $base = base_path();
+//
+    $contact_text = variable_get("ilot_theme_contact_text");
+
+//    dpm($newsletter_text);
+
+//    $file = file_load($visuel_offre);
+//    $img = image_style_url("header", $file->uri);
+
+?>
+
+<div class="bloc-contact">
+
+    <h1>Newsletter</h1>
+    <?php print $contact_text['value'];?>
+
+    <div class="contact-btn">Envoyez - nous !</div>
+    <div class="clear"></div>
+
+</div>
--- sites/all/modules/custom/ilot_theme/templates/ilot_theme_newsletter.tpl.php
+++ sites/all/modules/custom/ilot_theme/templates/ilot_theme_newsletter.tpl.php
@@ -2,7 +2,10 @@
 
 //    $base = base_path();
 //
-//    $newsletter_text = variable_get("ateliersmod_fsvisuels_offres");
+    $newsletter_text = variable_get("ilot_theme_newsletter_text");
+
+//    dpm($newsletter_text);
+
 //    $file = file_load($visuel_offre);
 //    $img = image_style_url("header", $file->uri);
 
@@ -10,6 +13,14 @@
 
 <div class="bloc-newsletter">
 
-TESTOUILLE
+    <h1>Newsletter</h1>
+    <?php print $newsletter_text['value'];?>
+
+    <div class="newsletter-form">
+    <?php $form = node_load(271);
+          $view = node_view($form);
+          print render($view); ?>
+    </div>
+    <div class="clear"></div>
 
 </div>
