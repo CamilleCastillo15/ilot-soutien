b44f292e76929fddba49e915d891750b0c806ff8
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-07-28 12:55:30 +0200

    wip de la home

---
+++ sites/all/modules/custom/ilot_theme/templates/ilot_theme_newsletter.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+//    $base = base_path();
+//
+//    $newsletter_text = variable_get("ateliersmod_fsvisuels_offres");
+//    $file = file_load($visuel_offre);
+//    $img = image_style_url("header", $file->uri);
+
+?>
+
+<div class="bloc-newsletter">
+
+TESTOUILLE
+
+</div>
