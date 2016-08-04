03a9c57ef73d51e16547c76934836e52940e0acc
michael tati <michael tati>
2016-08-04 17:47:16 +0200

    css

--- sites/all/themes/ilot/templates/node/node--mission.tpl.php
+++ sites/all/themes/ilot/templates/node/node--mission.tpl.php
@@ -7,6 +7,7 @@
  * @see https://drupal.org/node/1728164
  */
 ?>
+<div class="container-page">
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
 <div class="icone-set">
@@ -22,4 +23,5 @@
 <aside class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 <?php print render($content['field_missions_blocs']); ?>
 </aside>
+</div>
 <div class="clearfix"></div>
