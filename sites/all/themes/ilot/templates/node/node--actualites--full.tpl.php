d32017629d27bd0a6debde74094d93bbca1a25de
michael tati <michael tati>
2016-08-05 12:27:19 +0200

    css

--- sites/all/themes/ilot/templates/node/node--actualites--full.tpl.php
+++ sites/all/themes/ilot/templates/node/node--actualites--full.tpl.php
@@ -1,39 +1,40 @@
+<div class="container-page">
+  <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <div class="vignette">
-    <?php  print render($content['field_actualite_image_accroche']); ?>
-  </div>
-  <div class="texte">
-    <div class="auteur"><?php if(!empty($auteur)){print $auteur;} ?></div>
-    <h1><?php print $node->title; ?></h1>
-  </div>
-  <?php print $actualites_date; ?>
-  <div class="icone-set">
-    <?php print render($content['print_links']);?>
-    <?php print render($content['sharethis']);?>
-  </div>
-  <div class="corps">
-    <?php  print render($content['field_actualite_accroche']); ?>
-    <?php  print render($content['field_actualite_corps']); ?>
-    <?php if (isset($content['field_actualite_video'])): ?>
-      <h2>Intervention</h2>
-      <?php print render($content['field_actualite_video']); ?>
-    <?php endif; ?>
-     <div class="auteur"><?php if(!empty($auteur)){print $auteur;} ?></div>
-    <?php if (isset($content['field_actualite_auteur_fonction'])): ?>
-      <?php print render($content['field_actualite_auteur_fonction']); ?>
-    <?php endif; ?>
-  </div>
-  <div class="nav-next-prev">
-    <?php if(isset($prev)) :
-      print l('actualité précédente', 'node/'.$prev, array('attributes' => array('class' => 'prev read-more')));
-      endif;
-    ?>
-    <?php if(isset($next)) :
-      print l('actualité suivante', 'node/'.$next, array('attributes' => array('class' => 'next read-more')));
-      endif;
-    ?>
-  </div>
-</article><!-- /.node -->
+    <div class="vignette">
+      <?php  print render($content['field_actualite_image_accroche']); ?>
+    </div>
+    <div class="texte">
+      <div class="auteur"><?php if(!empty($auteur)){print $auteur;} ?></div>
+      <h1><?php print $node->title; ?></h1>
+    </div>
+    <?php print $actualites_date; ?>
+    <div class="icone-set">
+      <?php print render($content['print_links']);?>
+      <?php print render($content['sharethis']);?>
+    </div>
+    <div class="corps">
+      <?php  print render($content['field_actualite_accroche']); ?>
+      <?php  print render($content['field_actualite_corps']); ?>
+      <?php if (isset($content['field_actualite_video'])): ?>
+        <h2>Intervention</h2>
+        <?php print render($content['field_actualite_video']); ?>
+      <?php endif; ?>
+       <div class="auteur"><?php if(!empty($auteur)){print $auteur;} ?></div>
+      <?php if (isset($content['field_actualite_auteur_fonction'])): ?>
+        <?php print render($content['field_actualite_auteur_fonction']); ?>
+      <?php endif; ?>
+    </div>
+    <div class="nav-next-prev">
+      <?php if(isset($prev)) :
+        print l('actualité précédente', 'node/'.$prev, array('attributes' => array('class' => 'prev read-more')));
+        endif;
+      ?>
+      <?php if(isset($next)) :
+        print l('actualité suivante', 'node/'.$next, array('attributes' => array('class' => 'next read-more')));
+        endif;
+      ?>
+    </div>
+  </article><!-- /.node -->
+</div>
 <div class="clearfix"></div>
--- sites/all/themes/ilot/templates/node/node--actualites--teaser.tpl.php
+++ sites/all/themes/ilot/templates/node/node--actualites--teaser.tpl.php
@@ -1,19 +1,18 @@
 <article class="actus teaser node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="offres-speciales-header">
-      <div class="vignette img-actu">
+
+      <div class="vignette">
         <a href="<?php print url('node/' . $node->nid); ?>">
-        <?php  print render($content['field_actualite_image_accroche']); ?>
+          <?php  print render($content['field_actualite_image_accroche']); ?>
         </a>
-        <!-- <span class="auteur"><?php //if(!empty($auteur)){print $auteur;} ?></span> -->
       </div>
       <div class="date">
-            <?php print $actualites_date; ?>
+         <?php print $actualites_date; ?>
          <div class="icone-set socials">
             <?php print render($content['print_links']);?>
             <?php print render($content['sharethis']);?>
          </div>
       </div>
-      <div class="text-actus texte">
+      <div class="texte">
         <h1><?php print l($node->title, 'node/'.$node->nid, array('html' => TRUE)); ?></h1>
         <div class="text-detail">
             <?php  /*
@@ -28,9 +27,8 @@
             print render($content['field_actualite_accroche']);?>
 
         </div>
-        <div class="readmore">
-            <?php print l('voir plus', 'node/'.$node->nid, array('html' => TRUE, 'attributes' => array('class' => array('read-more')))); ?>
-        </div>
+   <?php print l("lire la suite", 'node/'.$node->nid, array("html"=>true, 'attributes' => array('class' => array('readmore')))); ?>
       </div>
-  </div>
+     
+    
 </article><!-- /.node -->
