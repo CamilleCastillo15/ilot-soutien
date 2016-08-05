417b40c4ff0c0e52c9b9e191135a37d7de54bf65
michael tati <michael tati>
2016-08-05 10:57:00 +0200

    css

--- sites/all/themes/ilot/js/ilot.conseil.js
+++ sites/all/themes/ilot/js/ilot.conseil.js
@@ -6,6 +6,7 @@
 
     	var currentItem = null;
     	var currentBt = null;
+    	var desti = "top";
 
          $('.img-admin').click(function(){
 	    	
@@ -13,13 +14,25 @@
          	if(currentItem != null){
          		currentItem.removeClass("active");
          	}
+
          	if(currentBt != null){
          		currentBt.removeClass("active");
+         		$(".details_conseil").removeClass(desti);
          	}
+
          	
          	var go = $("#"+$(this).attr('id')+"_textes");
          	go.addClass("active");
          	$(this).addClass("active");
+
+         	var _y = $(this).offset().top;
+         	if(_y < 700){
+         		desti = "top";
+         	}else{
+         		desti = "bottom";
+         	}
+
+         	$(".details_conseil").addClass(desti);
 
          	currentBt = $(this);
 
