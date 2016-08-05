35b22398820e7f5807a0a58fbea8ea5de9bd1f6f
michael tati <michael tati>
2016-08-05 16:16:03 +0200

    css

--- sites/all/themes/ilot/templates/node/node--liste-temoignage.tpl.php
+++ sites/all/themes/ilot/templates/node/node--liste-temoignage.tpl.php
@@ -1,6 +1,7 @@
+<div class="container-page full">
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $views_liste_temoignage; ?>
 
-</article><!-- /.node -->
-<div class="clearfix"></div>
+</article>
+</div>
