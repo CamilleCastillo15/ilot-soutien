6034da0a4d76d78238b928c0ab91ca0ac4cb3481
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-08-12 12:32:54 +0200

    wiwip

---
+++ sites/all/themes/ilot/templates/node/node--70.tpl.php
@@ -0,0 +1,30 @@
+<?php
+    $visuel_asso = variable_get("ilot_theme_nous_soutenir_visuel");
+    $file = file_load($visuel_asso);
+//    $img = file_create_url($file->uri);
+    $img = image_style_url("header", $file->uri);
+?>
+
+<div class="imagetitle">
+    <div class="image">
+        <img src="<?php print $img; ?>" alt="header" title="header" />
+    </div>
+    <div class="title">Nos missions</div>
+</div>
+
+<div class="container-page">
+
+    <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+      <h1><?php print $node->title; ?></h1>
+      <div class="icone-set">
+        <?php print render($content['print_links']);?>
+        <?php print render($content['sharethis']);?>
+      </div>
+      <?php
+        print render($content);
+      ?>
+    </article>
+
+</div>
+
