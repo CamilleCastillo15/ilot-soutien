2ee761a8ad83f51025d24eecc6be852f970617ad
Alexis Clairet <alexis@ladydinde.com>
2016-08-12 12:31:53 +0200

    formulaire

--- sites/all/modules/custom/commerce_french_donate/commerce_french_donate.js
+++ sites/all/modules/custom/commerce_french_donate/commerce_french_donate.js
@@ -1,6 +1,6 @@
 /**
  * @file A JavaScript file for the theme.
- * 
+ *
  * In order for this JavaScript to be loaded on pages, see the instructions in
  * the README.txt next to this file.
  */
@@ -11,10 +11,32 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function($, Drupal, window, document, undefined) {
   window.onunload = function(){};
-  $(function() {
 
-  
+  $(function() {
     var timer_ajax_fieldtext;
+    $('#calcu_dedu').keyup(function(value){
+      var v = Math.min(37500,parseInt($(this).val()));
+      var dedu = Math.floor(v*100/75);
+      $(this).val(v);
+      $('#calcu_don').val(dedu);
+    })
+    $('#calcu_dedu').change(function(value){
+      var v = parseInt($(this).val());
+      var dedu = Math.floor(v*100/75);
+      $('#calcu_don').val(dedu);
+
+
+    })
+    $('#calcu_don').keyup(function(value){
+      var v = Math.min(50000,parseInt($(this).val()));
+      var dedu = Math.floor(v*75/100);
+      $('#calcu_dedu').val(dedu);
+    })
+    $('#calcu_don').change(function(value){
+      var v = Math.min(50000,parseInt($(this).val()));
+      var dedu = Math.floor(v*75/100);
+      $('#calcu_dedu').val(dedu);
+    })
     $('body').on('keyup', 'div input.don.free',function() {
       $this = $(this);
       clearTimeout(timer_ajax_fieldtext);
@@ -57,5 +79,5 @@
       });
     }
   };
-  
+
 })(jQuery, Drupal, this, this.document);
